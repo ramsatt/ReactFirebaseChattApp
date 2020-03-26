899f8e0c17e40dac2843a11b700426537b3ceb3e
ramsatt@gmail.com <ramsatt@gmail.com>
2020-03-26 17:12:39 +0530

    BUGFix

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -59,7 +59,7 @@ export default class Login extends Component {
           <h1>
             Login to
             <Link className="title ml-2" to="/">
-              Chatty
+              Chat App
             </Link>
           </h1>
           <p className="lead">
